4f124fb824718502b21ed69e756f4bc3febe9e74
macdolven c. ekeh <dovenmark@gmail.com>
2019-04-08 13:35:00 +0000

    tutorial 6

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,71 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+##admin.site.register(Author)
+admin.site.register(Genre)
+##admin.site.register(BookInstance)
+##admin.site.register(Genre)
+
+
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
+    model = BookInstance
+
+
+class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+# admin.site.register(Book, BookAdmin)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_filter = ('status', 'due_back',)
+    # fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back',)
+        }),
+    )
